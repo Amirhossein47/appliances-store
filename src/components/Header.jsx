2bb39a4b26e4975c3e12ad3a760a8eecde7f6a00
Amirhossein47 <amirhosseinjodeyri47@gmail.com>
2024-04-28 23:49:49 +0330

    complete ProductListing Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <div>
+            <h1
+                className="leading-relaxed font-primary font-extrabold text-4xl text-center
+                text-palette-primary mt-4 py-2 sm:py-4"
+            >
+                خرید بهترین لوازم خانگی
+            </h1>
+            <p className="max-w-xl text-center px-2 mx-auto text-base text-gray-600">
+                بهترین کیفیت و برند لوازم خانگی را می توانید از ما خریداری کنید
+                🤗
+            </p>
+        </div>
+    );
+};
+
+export default Header;

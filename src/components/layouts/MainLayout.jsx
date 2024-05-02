e7d1b0f75de2e714c0be038fea0cd4088ef86c76
Amirhossein47 <amirhosseinjodeyri47@gmail.com>
2024-05-02 16:06:44 +0330

    02/05/2024

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
 import Navbar from "../NavBar";
 
 const MainLayout = ({ children }) => {
-    return (
-        <div className="flex flex-col min-h-s">
-            <Navbar />
-            <main>{children}</main>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-s">
+      <Navbar />
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default MainLayout;

da74977f25185f9cad9584ebcb8c9d48bcd67cf8
Amirhossein47 <amirhosseinjodeyri47@gmail.com>
2024-05-04 19:38:54 +0330

    complete pagination products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,66 @@
+import { useState } from "react";
+
 import { Helmet } from "react-helmet";
+import ReactPaginate from "react-paginate";
 import MainLayout from "./components/layouts/MainLayout";
 import Header from "./components/Header";
 import ProductListing from "./components/ProductListing";
+import { useSelector } from "react-redux";
 
-function App() {
-    return (
-        <MainLayout>
-            <Helmet>
-                <title>فروشگاه لوازم خانگی</title>
-            </Helmet>
+const PaginateItems = ({ productsPerPage, products, status }) => {
+  const [itemOffset, setItemOffset] = useState(0);
 
-            <div className="mx-auto max-w-6xl">
-                <Header />
-                <ProductListing />
-            </div>
-        </MainLayout>
-    );
-}
+  const endOffset = itemOffset + productsPerPage;
+
+  const currentProducts = products.slice(itemOffset, endOffset);
+  console.log(`currentProducts: ${currentProducts}`);
+
+  const pageCount = Math.ceil(products.length / productsPerPage);
+
+  const handlePageClick = (e) => {
+    const newOffset = e.selected * productsPerPage;
+
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      <ProductListing currentProducts={currentProducts} status={status} />
+      <ReactPaginate
+        containerClassName="flex justify-center items-center mt-8 mb-4"
+        pageClassName="block border border-solid border-lightgray w-10 h-10 flex items-center justify-center rounded-md mr-2"
+        activeClassName="bg-palette-primary text-palette-light hover:bg-palette-dark"
+        breakLabel="..."
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel={null}
+        nextLabel={null}
+        renderOnZeroPageCount={null}
+      />
+    </>
+  );
+};
+
+const App = () => {
+  const { items: products, status } = useSelector((state) => state.products);
+
+  return (
+    <MainLayout>
+      <Helmet>
+        <title>فروشگاه لوازم خانگی</title>
+      </Helmet>
+
+      <div className="mx-auto max-w-6xl">
+        <Header />
+        <PaginateItems
+          productsPerPage={9}
+          products={products}
+          status={status}
+        />
+      </div>
+    </MainLayout>
+  );
+};
 
 export default App;
--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -3,16 +3,14 @@ import { useSelector } from "react-redux";
 import { useGetAllProductsQuery } from "../slices/productApi";
 import ProductCard from "./ProductCard";
 
-const ProductListing = () => {
-  const { status } = useSelector((state) => state.products);
-
-  const { data } = useGetAllProductsQuery();
+const ProductListing = ({ currentProducts, status }) => {
+  // const { data } = useGetAllProductsQuery();
 
   return (
     <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
       {status === "success" ? (
         <>
-          {data?.map((product, index) => (
+          {currentProducts?.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
         </>
